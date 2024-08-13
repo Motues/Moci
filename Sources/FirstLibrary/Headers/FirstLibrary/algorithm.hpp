5978a4ab8f9a628ef20342b696630e510decb490
Sango <letusgoone@outlook.com>
2024-08-13 22:14:18 +0800

    构造时尽可能直接构造成员 使用 std::initialize_list 初始化成员 修改构造函数中成员构造的顺序，避免与直觉不符合的构造调用顺序

--- Sources/FirstLibrary/Headers/FirstLibrary/algorithm.hpp
+++ Sources/FirstLibrary/Headers/FirstLibrary/algorithm.hpp
@@ -6,27 +6,28 @@
 namespace Moci {
     constexpr int INF = 1e9;
     using PII = std::pair<int, int>;
-    typedef std::pair<int, int> PII;
-
 
 	class Graph {
     public:
         void addEdge(int from, int to, int cost);
         void dijkstra(int start);
         void print();
-        Graph(int numVertex) : numVertex(numVertex), head(numVertex, -1), dis(numVertex, INF),cnt(0) {
-            Edge edge = {0, 0, 0};
-            edges.push_back(edge);
-        };
+
+        explicit Graph(const int numVertex) :
+			head(numVertex, -1),
+			dis(numVertex, INF),
+			numVertex(numVertex) {
+        }
 
     private:
         struct Edge {
             int to, next, cost;
         };
-        std::vector<Edge> edges;
+        std::vector<Edge> edges{{0,0,0}};
         std::vector<int> head;
         std::vector<int> dis;
-        int cnt, numVertex;
+        int cnt{0};
+		int numVertex;
 
     };
 }
